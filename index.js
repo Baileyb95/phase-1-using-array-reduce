0e3ead7eea2a0762e7a47a033e9626530d28463d
Bailey Bell Loffredo <baileybell.loffredo@gmail.com>
2023-08-06 19:31:55 -0400

    complete

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-
 // Code your solution here
+function reducer(totalBatteries, battery) {
+  return totalBatteries + battery;
+
+}
+function totalBatteries(batteryBatches) {
+    totalBatteries = batteryBatches.reduce(reducer, 0)
+    return batteryBatches.reduce(reducer, 0);
+    }
+const total = totalBatteries(batteryBatches)
